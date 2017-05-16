5ceb24efe17755798e5a10057131dcc72a3df1e0
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 20:10:56 +0100

    Pequeños cambios

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,23 @@
+var Fibonacci = require("../lib/Fibonacci.js");
+var should = require("should")
+
+describe("Función comparar", function(){
+  it("No existen secuencias declaradas", function() {
+    var mySecuencia = new Fiboancci(5);
+    var result = mySecuencia.getSecuencia();
+    result.should.be.equal("0, 1, 1, 2, 3");
+  }
+  it("No existen operaciones declaradas", function() {
+    var mySecuencia = new Secuencia(5);
+    var prueba = new Fibonacci(5);
+    var result = mySecuencia.operar(prueba, "Igual")
+    result.should.be.equal("0, 1, 1, 2, 3");
+  }
+
+})
+
+
+/*
 var assert = chai.assert;
 
 suite('Fibonacci', function() {
@@ -13,3 +33,4 @@ suite('Fibonacci', function() {
     });
 
 });
+*/
